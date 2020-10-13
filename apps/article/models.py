70f0c0cd93858ddc59272d3a84b641cb7827cfde
James-zeng <70133866+zqy15782@users.noreply.github.com>
2020-10-13 19:28:39 +0800

    实现频道列表接口

--- apps/article/models.py
+++ apps/article/models.py
@@ -5,22 +5,21 @@ from django.db import models
 # Create your models here.
 from question.models import Label
 
-
 User = get_user_model()
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=100, null=True, default=None, verbose_name="标题")
     content = RichTextUploadingField(default='', verbose_name='文章内容')
     image = models.CharField(null=True, max_length=100, default=None, verbose_name="文章封面")
-    createtime = models.DateTimeField(auto_now_add=True, null=True,verbose_name="创建日期")
-    updatetime = models.DateTimeField(auto_now=True, null=True,verbose_name="修改日期")
+    createtime = models.DateTimeField(auto_now_add=True, null=True, verbose_name="创建日期")
+    updatetime = models.DateTimeField(auto_now=True, null=True, verbose_name="修改日期")
     visits = models.IntegerField(null=True, default=0, verbose_name="浏览量")
     thumbup = models.IntegerField(null=True, default=0, verbose_name="点赞数")
     comment_count = models.IntegerField(null=True, default=0, verbose_name="评论数")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles', verbose_name='用户ID')
-    channel = models.ForeignKey("Channel", related_name="articles", on_delete=models.CASCADE, null=True, default=None,verbose_name="所属频道")
+    channel = models.ForeignKey("Channel", related_name="articles", on_delete=models.CASCADE, null=True, default=None,
+                                verbose_name="所属频道")
     labels = models.ManyToManyField(Label, symmetrical=False, related_name='articles')  # 这个问题属于哪些标签的
     collected_users = models.ManyToManyField(User, symmetrical=False, related_name='collected_articles')  # 被哪些用户收藏
 
@@ -35,7 +34,6 @@ class Article(models.Model):
 
 
 class Channel(models.Model):
-
     name = models.CharField(max_length=100, null=True, default=None, verbose_name="频道名称")
 
     class Meta:
@@ -48,12 +46,13 @@ class Channel(models.Model):
 
 
 class Comment(models.Model):
-
-    content = models.CharField(max_length=1000,default='', verbose_name='回答内容')
+    content = models.CharField(max_length=1000, default='', verbose_name='回答内容')
     createtime = models.DateTimeField(auto_now_add=True, null=True, verbose_name="创建日期")
     updatetime = models.DateTimeField(auto_now=True, null=True, verbose_name="修改日期")
-    parent = models.ForeignKey('self', on_delete=models.SET_NULL, related_name='subs', null=True, blank=True, verbose_name='父评论')
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments", default=None,verbose_name="问题ID")
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, related_name='subs', null=True, blank=True,
+                               verbose_name='父评论')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments", default=None,
+                                verbose_name="问题ID")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments", default=None, verbose_name="用户ID")
 
     class Meta:
