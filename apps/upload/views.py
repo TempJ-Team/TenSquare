26b197ebeca6231b11807998fb6a1dc4667ff830
SmartFox <fairyfox97@gmail.com>
2020-10-15 11:21:45 +0800

    修复图片上传的BUG

--- apps/upload/views.py
+++ apps/upload/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.http.response import HttpResponseRedirect
+from django.shortcuts import redirect
 from utils.Qiniu_utils import imAge_upload
 from rest_framework import status
 from config.config import eXt_List
@@ -11,22 +11,22 @@ from config.config import eXt_List
 class RichEditorImageUpload(APIView):
 
     def post(self, request):
-        _img = request.FILES.get('img')
-
+        _img = request.data
+        print(_img)
+        _img = request.data.get('upload')
         if _img:
             ext = _img.name.split('.')[-1]
-
+            print(_img)
             if ext not in eXt_List:
                 return Response({
                     'msg': '文件类型不允许！'
                 }, status=status.HTTP_400_BAD_REQUEST)
 
+        print(1)
         image_url = imAge_upload(_img)
 
         if image_url:
-            return Response({
-                'imgurl': image_url,
-            }, status=status.HTTP_201_CREATED)
+            return redirect(image_url)
         return Response({
             'msg': '服务器内部错误！'
         }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -35,7 +35,9 @@ class RichEditorImageUpload(APIView):
 class AvatarImageUpload(APIView):
 
     def post(self, request):
-        _img = request.FILES.get('upload')
+        _img = request.data
+        print(_img)
+        _img = request.data.get('img')
         if _img:
             ext = _img.name.split('.')[-1]
 
