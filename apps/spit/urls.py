eb5a197ce542ec0031d0da640f89208d8d01064c
zjy0828 <70137551+zjy0828@users.noreply.github.com>
2020-10-15 11:24:36 +0800

    优化代码

--- apps/spit/urls.py
+++ apps/spit/urls.py
@@ -3,12 +3,10 @@ from .views import *
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
-    # re_path(r'^search/$', views.MySearchView()),
     re_path(r'^spit/$', SpitSimpleView.as_view({'get':'list','post':'create'})),
     re_path(r'^spit/(?P<pk>\d+)/$', SpitSimpleView.as_view({'get':'retrieve'})),
-    re_path(r'^spit/(?P<id>\d+)/collect/$', SpitCollectView.as_view()),
-    re_path(r'^spit/(?P<id>\d+)/updatethumbup/$', SpitHasthubupView.as_view()),
+    re_path(r'^spit/(?P<id>\d+)/collect/$', SpitSimpleView.as_view({'put':'collect'})),
+    re_path(r'^spit/(?P<id>\d+)/updatethumbup/$', SpitSimpleView.as_view({'put':'updatehastumbup'})),
     re_path(r'^spit/(?P<pk>\d+)/children/$', SpitCommentView.as_view()),
-    # re_path(r'^authorizations/$', obtain_jwt_token),
 ]
 
