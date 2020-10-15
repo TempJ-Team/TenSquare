4c860a98cbb893b1416383815f5f96c024eeb70d
SmartFox <fairyfox97@gmail.com>
2020-10-15 15:13:29 +0800

    修复bug

--- apps/question/serializers.py
+++ apps/question/serializers.py
@@ -1,5 +1,18 @@
 from rest_framework import serializers
 from .models import *
+
+# 个人提问
+class LablesModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Label
+        fields = '__all__'
+
+class QuestionsModelSerializer(serializers.ModelSerializer):
+    lables = LablesModelSerializer(many=True)
+
+    class Meta:
+        model = Question
+        fields = '__all__'
 #所有标签
 class  LabelAllModelSerializer(serializers.ModelSerializer):
     class Meta:
